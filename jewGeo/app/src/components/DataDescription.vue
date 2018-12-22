5b56b6f1c476d1fe12180f8aa2151fae306d79c0
sol laudon <blaudon1@gmail.com>
2018-12-22 10:55:07 -0700

    componentize each data point

---
+++ jewGeo/app/src/components/DataDescription.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="description-box">
+        <div class="single-data-point">Label: {{mapLabel}} </div>
+        <div class="single-data-point">Country: {{country}} </div>
+        <div class="single-data-point">City: {{city}} </div>
+        <div class="single-data-point">Population: {{population}} </div>
+        <div class="single-data-point">Year: {{year}} </div>
+        <div class="single-data-point">Source: {{source}} </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DataDescription',
+        props: {
+            mapLabel: {
+                type: String,
+                required: true
+            },
+            year: {
+                type: String,
+                required: true
+            },
+            population: {
+                type: String,
+                required: true
+            },
+            country: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            source: {
+                type: String,
+                required: true
+            },
+        }
+    }
+</script>
+
+<style>
+    .description-box {
+        width: 300px;
+        border: black solid 1px;
+        margin: 10px;
+    }
+    .single-data-point {
+    text-align: left;
+    }
+</style>
--- jewGeo/app/src/components/DataList.vue
+++ jewGeo/app/src/components/DataList.vue
@@ -3,37 +3,31 @@
         <div>Full Data List</div>
         <div :v-if="geoData.length > 0">
             <div v-for="dataPoint in geoData">
-                <div class="single-data-point"> Year: {{ dataPoint.year}} </div>
-                <div class="single-data-point"> Country: {{ dataPoint.country}} </div>
-                <div class="single-data-point"> City: {{ dataPoint.city}} </div>
-                <div class="single-data-point"> Population Size: {{ dataPoint.population}} </div>
-                <div class="single-data-point"> Source: {{ dataPoint.source}} </div>
+                <DataDescription
+                    :mapLabel="dataPoint.label"
+                    :year="dataPoint.year"
+                    :population="dataPoint.population"
+                    :country="dataPoint.country"
+                    :city="dataPoint.city"
+                    :source="dataPoint.source"
+                ></DataDescription>
             </div> 
-            <!-- <ul>
-                <li v-for="dataPoint in geoData">
-                        <div> Year: {{ dataPoint.year}} </div>
-                        <div> Country: {{ dataPoint.country}} </div>
-                        <div> City: {{ dataPoint.city}} </div>
-                        <div> Population Size: {{ dataPoint.population}} </div>
-                        <div> Source: {{ dataPoint.source}} </div>
-                </li>
-            </ul> -->
         </div>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import DataDescription from './DataDescription.vue'
+
     export default {
         name: 'DataList',
-         computed: {
+        components: {
+            DataDescription
+        },
+        computed: {
             ...mapState(['geoData'])
         }
-        // data() {
-        //     return {
-        //         geographicdata: []
-        //     }
-        // }
     }
 </script>
 
